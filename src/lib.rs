b8f7a7d18840edbcc838d5820148d3da69e02321
Maximilian Heisinger <maximilian.heisinger@jku.at>
2023-01-31 14:15:10 +0100

    Working parsing and constraint extraction

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,20 @@ pub enum IntegerSplitKind {
 }
 
 #[derive(Debug, Clone)]
-pub struct IntegerSplit {
+pub struct IntegerSplitConstraint {
     kind: IntegerSplitKind,
-    vars: Vec<i32>,
     target: Vec<Vec<i32>>,
 }
 
 #[derive(Debug, Clone)]
+pub struct IntegerSplit {
+    vars: Vec<i32>,
+    constraints: Vec<IntegerSplitConstraint>,
+}
+
+#[derive(Debug, Clone)]
 pub struct Formula {
+    pub splits: Vec<IntegerSplit>,
     pub prefix: Vec<i32>,
     pub matrix: Vec<Vec<i32>>,
     pub nr_of_variables: i32,
@@ -80,6 +86,11 @@ pub fn write_qdimacs(tgt: &str, formula: &Formula) -> std::io::Result<()> {
     Ok(())
 }
 
+fn var_constraint_to_nr_of_bits(v: i32) -> i32 {
+    let vf: f64 = v as f64;
+    vf.log2().ceil() as i32
+}
+
 pub fn parse_qdimacs(qdimacs: &str) -> Result<Formula, String> {
     let file = QDIMACSParser::parse(Rule::file, qdimacs)
         .expect("parser error")
@@ -90,7 +101,7 @@ pub fn parse_qdimacs(qdimacs: &str) -> Result<Formula, String> {
     let mut nr_of_clauses: i32 = 0;
     let mut prefix: Vec<i32> = vec![];
     let mut matrix: Vec<Vec<i32>> = vec![];
-    let mut integer_splits: Vec<IntegerSplit> = vec![];
+    let mut splits: Vec<IntegerSplit> = vec![];
 
     for line in file.into_inner() {
         match line.as_rule() {
@@ -101,58 +112,81 @@ pub fn parse_qdimacs(qdimacs: &str) -> Result<Formula, String> {
                 nr_of_clauses = inner_rules.next().unwrap().as_str().parse::<i32>().unwrap();
             }
             Rule::int_split_line => {
+                let mut constraints: Vec<IntegerSplitConstraint> = vec![];
+
                 let mut inner_rules = line.into_inner();
                 let mut vars_or_cmp = inner_rules.next().unwrap();
                 let mut vars_or_cmp_rule = vars_or_cmp.as_rule();
                 let mut vars: Vec<i32> = vec![];
-                let mut target: Vec<Vec<i32>> = vec![];
+                let mut nr_of_bits = 0;
                 while vars_or_cmp_rule == Rule::pnum {
                     let v = vars_or_cmp.as_str().parse::<i32>().unwrap();
                     vars.push(v);
-                    println!("V: {}", v);
                     vars_or_cmp = inner_rules.next().unwrap();
                     vars_or_cmp_rule = vars_or_cmp.as_rule();
                 }
 
-                let kind: IntegerSplitKind = match vars_or_cmp.as_str() {
-                    "<" => IntegerSplitKind::LessThan,
-                    ">" => IntegerSplitKind::GreaterThan,
-                    "=" => IntegerSplitKind::Equals,
-                    _ => panic!("Unknown pattern!"),
-                };
+                while vars_or_cmp.as_rule() == Rule::cmp {
+                    let mut target: Vec<Vec<i32>> = vec![];
+                    let kind: IntegerSplitKind = match vars_or_cmp.as_str() {
+                        "<" => IntegerSplitKind::LessThan,
+                        ">" => IntegerSplitKind::GreaterThan,
+                        "=" => IntegerSplitKind::Equals,
+                        _ => panic!("Unknown pattern!"),
+                    };
 
-                if matches!(kind, IntegerSplitKind::Equals) {
-                    let mut num = inner_rules.next();
-                    while num.is_some() {
-                        target.push(
-                            num.unwrap()
-                                .as_str()
-                                .chars()
-                                .map(|x| {
-                                    if x == '1' {
-                                        1
-                                    } else if x == '0' {
-                                        0
-                                    } else {
-                                        panic!(
+                    if matches!(kind, IntegerSplitKind::Equals) {
+                        let mut next = inner_rules.next();
+                        while next.is_some() {
+                            let next_ = next.unwrap();
+                            if next_.as_rule() == Rule::onezero {
+                                target.push(
+                                    next_
+                                        .as_str()
+                                        .chars()
+                                        .map(|x| {
+                                            if x == '1' {
+                                                1
+                                            } else if x == '0' {
+                                                0
+                                            } else {
+                                                panic!(
                                             "Integer Split with = must only have 1 and 0 as match!"
                                         );
+                                            }
+                                        })
+                                        .collect(),
+                                );
+                                if nr_of_bits == 0 {
+                                    nr_of_bits = target[0].len();
+                                } else {
+                                    if target[target.len() - 1].len() != nr_of_bits {
+                                        panic!("Number of assigned bits in equals must always be the same!");
                                     }
-                                })
-                                .collect(),
-                        );
-                        num = inner_rules.next();
+                                }
+                            } else {
+                                break;
+                            }
+                            next = inner_rules.next();
+                        }
+                    } else {
+                        let next = inner_rules.next();
+                        assert!(next.is_some());
+                        let next_ = next.unwrap();
+                        target.push(vec![next_.as_str().parse::<i32>().unwrap()]);
                     }
-                } else {
-                    target.push(vec![inner_rules
-                        .next()
-                        .unwrap()
-                        .as_str()
-                        .parse::<i32>()
-                        .unwrap()]);
-                }
 
-                integer_splits.push(IntegerSplit { kind, vars, target });
+                    let constraint = IntegerSplitConstraint { kind, target };
+                    constraints.push(constraint);
+
+                    let next = inner_rules.next();
+                    if next.is_some() {
+                        vars_or_cmp = next.unwrap();
+                    } else {
+                        break;
+                    }
+                }
+                splits.push(IntegerSplit { vars, constraints });
             }
             Rule::quant_set => {
                 let mut inner_rules = line.into_inner();
@@ -176,10 +210,41 @@ pub fn parse_qdimacs(qdimacs: &str) -> Result<Formula, String> {
         }
     }
 
+    // Fixup integer splits without assigned variables based on their properties.
+    let mut prefix_start = 0;
+    for s in splits.iter_mut() {
+        if s.constraints.len() < 1 {
+            panic!("Require some constraints for int splits!");
+        }
+        if s.vars.len() == 0 {
+            let nr_of_bits = match s.constraints[0].kind {
+                IntegerSplitKind::LessThan | IntegerSplitKind::GreaterThan => {
+                    var_constraint_to_nr_of_bits(s.constraints[0].target[0][0])
+                }
+                IntegerSplitKind::Equals => s.constraints[0].target[0].len() as i32,
+            };
+            s.vars =
+                prefix[(prefix_start as usize)..((prefix_start + nr_of_bits) as usize)].to_vec();
+        } else {
+            prefix_start += s.vars.len() as i32;
+        }
+    }
+
     Ok(Formula {
+        splits,
         prefix,
         matrix,
         nr_of_variables,
         nr_of_clauses,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_nr_to_bits() {
+        assert_eq!(var_constraint_to_nr_of_bits(5), 3);
+    }
 }
